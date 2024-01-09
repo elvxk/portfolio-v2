55b17623a635d6085cde795721e9d7a565bad154
elvxk <pradiptaramadan9c@gmail.com>
2024-01-10 06:01:46 +0700

    Fix Style Up 🦋

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,10 @@ const App = () => {
 
       <ScrollToTop
         style={{
+          background: "#2e2e2e",
           borderRadius: "100px",
         }}
-        className=" flex justify-center items-center text-brown bg-black hover:bg-blue"
+        className="flex justify-center items-center text-brown"
         component={<ion-icon name="chevron-up-outline"></ion-icon>}
         smooth
       />
