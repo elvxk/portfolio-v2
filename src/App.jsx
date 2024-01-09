b47228bea8ded45ea495ecf24f10e5021469db70
elvxk <pradiptaramadan9c@gmail.com>
2024-01-10 05:58:21 +0700

    Up lagi 🥱

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,12 @@ const App = () => {
       <Portfolio />
       <Contact />
       <Footer />
+
       <ScrollToTop
-        className="important h-8 w-8 text-brown rounded-full flex justify-center items-center bg-black/75 hover:bg-blue"
+        style={{
+          borderRadius: "100px",
+        }}
+        className=" flex justify-center items-center text-brown bg-black hover:bg-blue"
         component={<ion-icon name="chevron-up-outline"></ion-icon>}
         smooth
       />
